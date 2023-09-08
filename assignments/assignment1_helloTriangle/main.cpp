1a6da7c0bfacf0d708d4fe0559ea809427c6a331
IsaacLovy <isaac.lovy@mymail.champlain.edu>
2023-09-08 10:22:49 -0400

    finished tutorial for hello triangle

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -11,23 +11,30 @@ const int SCREEN_HEIGHT = 720;
 const char* vertexShaderSource = R"(
 #version 450
 layout(location = 0) in vec3 vPos;
+layout(location = 1) in vec4 vColor;
+out vec4 Color;
+uniform float _Time;
 void main(){
-	gl_Position = vec4(vPos,1.0);
+	Color = vColor;
+	vec3 offset = vec3(0,sin(vPos.x + _Time),0)*0.5f;
+	gl_Position = vec4(vPos + offset,1.0);
 }
 )";
 
 const char* fragmentShaderSource = R"(
 #version 450
 out vec4 FragColor;
+in vec4 Color;
+uniform float _Time;
 void main(){
-	FragColor = vec4(1.0,1.0,1.0,1.0);
+	FragColor = Color * abs(sin(_Time));
 }
 )";
 
-float vertices[9] = {
-	-0.5, -0.5, 0.0,
-	 0.5, -0.5, 0.0,
-	 0.0,  0.5, 0.0
+float vertices[21] = {
+	-0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 1.0,
+	 0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 1.0,
+	 0.0,  0.5, 0.0, 0.0, 0.0, 1.0, 1.0
 };
 
 unsigned int createVAO(float* vertexData, int numVertices)
@@ -35,15 +42,20 @@ unsigned int createVAO(float* vertexData, int numVertices)
 	unsigned int vbo;
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 3 * numVertices, vertexData, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 7 * numVertices, vertexData, GL_STATIC_DRAW);
 
 	unsigned int vao;
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
+	//Position Attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (const void*)0);
 	glEnableVertexAttribArray(0);
+
+	//Color Attribute
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (const void*)(sizeof(float) * 3));
+	glEnableVertexAttribArray(1);
 
 	return vao;
 }
@@ -118,8 +130,14 @@ int main() {
 		glfwPollEvents();
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
 		glUseProgram(shader);
 		glBindVertexArray(vao);
+
+		float time = (float)glfwGetTime();
+		int timeLocaton = glGetUniformLocation(shader, "_Time");
+		glUniform1f(timeLocaton, time);
+
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
